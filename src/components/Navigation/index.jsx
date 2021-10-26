70c3f4d31a5a7a23fcd8d8f598e7cefe38b960ce
Daris <daris.hodzic.dh@gmail.com>
2021-10-26 16:23:06 +0200

    navbar 1.0

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -3,11 +3,16 @@ import classes from "./style.module.scss";
 const Navigation = () => {
   return (
     <nav>
-      <div className={classes.logo}></div>
-      <div className={classes.searchBar}></div>
+      <div className={classes.logo}>LOGO</div>
+      <div className={classes.searchBar}>
+        <form>
+          <input type="text" />
+          <button type="submit">Search</button>
+        </form>
+      </div>
       <div className={classes.icon}>
         <i className="fas fa-shopping-cart"></i>
-        <i class="fas fa-user"></i>
+        <i className="fas fa-user"></i>
       </div>
     </nav>
   );
