05c0f3cadcd7142a74cef8f122c51202d4723154
Daris <daris.hodzic.dh@gmail.com>
2021-11-09 20:14:26 +0100

    fixed

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-import classes from "./style.module.scss";
+import styles from "./style.module.scss";
 const Navigation = () => {
   return (
     <nav>
-      <div className={classes.logo}>LOGO</div>
-      <div className={classes.searchBar}>
+      <div className={styles.logo}>LOGO</div>
+      <div className={styles.searchBar}>
         <form>
           <input type="text" />
           <button type="submit">Search</button>
         </form>
       </div>
-      <div className={classes.icon}>
+      <div className={styles.icon}>
         <i className="fas fa-shopping-cart"></i>
         <i className="fas fa-user"></i>
       </div>
